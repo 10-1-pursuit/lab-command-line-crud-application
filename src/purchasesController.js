374e2cda1fa2d0cee05026e41f280a6775cb5e99
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-03 11:09:29 -0400

    added a data and src folder with content inside

---
+++ src/purchasesController.js
@@ -0,0 +1,11 @@
+const inform = console.log
+const {nanoid} = require("nanoid")
+
+function create(purchases,purchaseName){
+    const newPurchase = {
+        id: nanoid(4),
+        name: purchaseName,
+        amount: 
+        donation: 
+    }
+}
