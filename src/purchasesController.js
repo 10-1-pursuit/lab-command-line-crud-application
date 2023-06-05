d89435f8f9a86adc609a0e038ab4a0fef785486e
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-05 13:05:36 -0400

    still working on view function and have to write tests

--- src/purchasesController.js
+++ src/purchasesController.js
@@ -1,68 +1,77 @@
-const inform = console.log
-const {nanoid} = require("nanoid")
-const {chalk} = require("chalk")
-const inventory = require("../data/inventory.json")
+const inform = console.log;
+const { nanoid } = require("nanoid");
+const chalk = require("chalk");
+const inventory = require("../data/inventory.json");
 
+function create(customerPurchases, purchaseItem) {
+  // customerPurchases = customerOrder.json array / purchaseItem is inventory.name
 
+  const newPurchase = {
+    id: `${nanoid(4)}`,
+    name: purchaseItem,
+    cost: inventory[purchaseItem].toFixed(2),
+    donation: Number(
+      (Math.ceil(inventory[purchaseItem]) - inventory[purchaseItem]).toFixed(2)
+    ),
+  };
+  customerPurchases.push(newPurchase);
+  return customerPurchases;
+}
 
-function create(customerPurchases, purchaseItem){ // customerPurchases = customerOrder.json array / purchaseItem is inventory.name 
-  
-    const newPurchase = {
-        id: `${nanoid(4)}`,
-        name: purchaseItem,
-        cost: inventory[purchaseItem].toFixed(2),
-        donation: Number(((Math.ceil(inventory[purchaseItem])) - inventory[purchaseItem]).toFixed(2))
-    }
-        customerPurchases.push(newPurchase)
-        return customerPurchases
+function index(customerPurchases) {
+  return customerPurchases.map(
+    (eachPurchase) => eachPurchase.id + " " + eachPurchase.name
+  );
+}
+
+function view(customerPurchases, purchaseItem) {
+  let singlePurchaseInfo = customerPurchases.filter(
+    (item) => item.name === purchaseItem)
+
+    return singlePurchaseInfo
 }
 
 
-function index(customerPurchases){
-    return customerPurchases.map((eachPurchase) => eachPurchase.id + " " + eachPurchase.name)
+function update(customerPurchases, purchaseId, updatedPurchase) {
+  const index = customerPurchases.findIndex(
+    (purchase) => purchase.id === purchaseId
+  );
+  if (index > -1) {
+    customerPurchases[index].id = purchaseId;
+    customerPurchases[index].name = updatedPurchase;
+    customerPurchases[index].cost = inventory[updatedPurchase];
+    customerPurchases[index].donation = Number(
+      (
+        Math.ceil(inventory[updatedPurchase]) - inventory[updatedPurchase]
+      ).toFixed(2)
+    );
+
+    inform("Purchase successfully updated");
+    return customerPurchases;
+  } else {
+    inform("Purchase not found. No action taken");
+    return customerPurchases;
+  }
 }
 
-function view(customerPurchases, purchaseItem){
-   let singlePurchaseInfo = create(customerPurchases, purchaseItem)
-  let greenKeys = chalk.green(Object.keys(singlePurchaseInfo))
-  let mixedKeys = chalk.yellow(greenKeys.donation)
-return mixedKeys
+function remove(customerPurchases, purchaseId) {
+  const index = customerPurchases.findIndex(
+    (purchase) => purchase.id === purchaseId
+  );
+  if (index) {
+    customerPurchases.splice(index, 1);
+    inform("Purchase successfully removed from your order!");
+    return customerPurchases;
+  } else {
+    inform("Purchase not found. No action taken");
+    return customerPurchases;
+  }
 }
 
-function update(customerPurchases, purchaseItem, updatedPurchase){
-
-        const index = customerPurchases.findIndex((purchase) => purchase.name === purchaseItem);
-        if (index > -1) {
-            customerPurchases[index].id = customerPurchases[id];
-            customerPurchases[index].name = updatedPurchase;
-            customerPurchases[index].cost = purchases[updatedPurchase];
-            customerPurchases[index].donation = purchases[updatedPurchase];
-
-            inform("Purchase successfully updated");
-            return customerPurchases;
-        } else {
-            inform("Purchase not found. No action taken");
-            return customerPurchases;
-        }
-    }
-
-function remove(customerPurchases, purchaseId){
-    const index = customerPurchases.findIndex((purchase) => purchase.id === purchaseId);
-    if (index > -1) {
-      customerPurchases.splice(index, 1);
-      inform("Purchase successfully removed from collection");
-      return customerPurchases;
-    } else {
-      inform("Purchase not found. No action taken");
-      return customerPurchases;
-    }
+function total(customerPurchases) {
+  let filteredDonations = customerPurchases.map((amounts) => amounts.donation);
+  let donationAmount = filteredDonations.reduce((prev, curr) => prev + curr);
+  return donationAmount;
 }
 
-
-function total(customerPurchase){
- return customerPurchase.reduce((prev, curr)=>prev.donation + curr.donation)
-
-}
-
-
-module.exports = {create, index, view, update, remove, total}
+module.exports = { create, index, view, update, remove, total };
