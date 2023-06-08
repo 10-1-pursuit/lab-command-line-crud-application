0e8581def012bf56f494cbd15ccac5fd592c82fc
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-06-08 00:55:03 -0400

    Finish 'create', 'index'.

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const { writeJSONFile, readJSONFile } = require('./src/helpers');
-const {
-  create,
-  destroy,
-  edit,
-  index,
-  show,
-  total
-} 
+const { create, index } = require('./src/purchasesController');
+
+const inform = console.log;
+
+function run() {
+	const action = process.argv[2];
+	const purchase = process.argv[3];
+	const amount = Number(process.argv[4]);
+	const donation = Number(process.argv[5]);
+	let purchases = readJSONFile('./data', 'purchases.json');
+	let writeToFile = false;
+	let updatedPurchases = [];
+
+	inform('Welcome to our Supreme Bread Bakery 🍞🥖🥐🥨🥯\n\n');
+
+	switch (action) {
+		case 'index':
+			const purchasesView = index(purchases);
+			inform(purchasesView);
+			break;
+		case 'create':
+			updatedPurchases = create(purchases, purchase, amount, donation);
+			writeToFile = true;
+			break;
+		default:
+			inform('There was an error.');
+	}
+	if (writeToFile) {
+		writeJSONFile('data', 'purchases.json', updatedPurchases);
+	}
+}
+
+run();
--- src/purchasesController.js
+++ src/purchasesController.js
@@ -1,2 +1,26 @@
 const { nanoid } = require('nanoid');
 const chalk = require('chalk');
+
+const inform = console.log;
+
+function create(purchases, purchaseName, amount, donation) {
+	const purchase = {
+		id: nanoid(4),
+		name: purchaseName,
+		amount: amount,
+		donation: donation,
+	};
+	purchases.push(purchase);
+	return purchases;
+}
+
+function index(purchases) {
+	return purchases
+		.map((purchase) => purchase.id + ' ' + purchase.name)
+		.join('\n');
+}
+
+module.exports = {
+	create,
+	index,
+};
