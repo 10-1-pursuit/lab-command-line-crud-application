3bb12075de76b076dc53de88a9a05cb246754d61
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-06-12 04:17:03 -0400

    Final submission.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const { writeJSONFile, readJSONFile } = require('./src/helpers');
-const { create, index, show } = require('./src/purchasesController');
+const {
+	create,
+	destroy,
+	edit,
+	index,
+	show,
+} = require('./src/purchasesController');
+
+const chalk = require('chalk');
 
 const inform = console.log;
 
 function run() {
 	const action = process.argv[2];
 	const purchase = process.argv[3];
-	const amount = Number(process.argv[4]);
-	const donation = Number(process.argv[5]);
+	const amount = process.argv[4];
+	const donation = process.argv[5];
 	let purchases = readJSONFile('./data', 'purchases.json');
 	let writeToFile = false;
 	let updatedPurchases = [];
@@ -17,7 +25,7 @@ function run() {
 	switch (action) {
 		case 'index':
 			const purchasesView = index(purchases);
-			inform(purchasesView);
+			inform(`${purchasesView}\n`);
 			break;
 		case 'create':
 			updatedPurchases = create(purchases, purchase, amount, donation);
@@ -26,6 +34,31 @@ function run() {
 		case 'show':
 			const purchaseView = show(purchases, purchase);
 			inform(purchaseView);
+			break;
+		case 'update':
+			updatedPurchases = edit(
+				purchases,
+				purchase,
+				process.argv[4],
+				process.argv[5],
+				process.argv[6]
+			);
+			writeToFile = true;
+			break;
+		case 'destroy':
+			updatedPurchases = destroy(purchases, purchase);
+			writeToFile = true;
+			break;
+		case 'total':
+			const total = purchases.reduce(
+				(acc, curr) => acc + Number(curr.donation),
+				0
+			);
+			inform(
+				`${chalk.yellow('Total donation amount:')} ${chalk.green(
+					'$'
+				)}${chalk.green(total.toFixed(2))}`
+			);
 			break;
 		default:
 			inform('There was an error.');
--- src/purchasesController.js
+++ src/purchasesController.js
@@ -7,8 +7,8 @@ function create(purchases, purchaseName, amount, donation) {
 	const purchase = {
 		id: nanoid(4),
 		name: purchaseName,
-		amount: amount,
-		donation: donation,
+		amount: Number(amount),
+		donation: Number(donation),
 	};
 	purchases.push(purchase);
 	return purchases;
@@ -29,8 +29,51 @@ function show(purchases, purchaseId) {
 	)} ${chalk.yellowBright('donation')} ${chalk.green(purchase.donation)}`;
 }
 
+function edit(
+	purchases,
+	purchaseId,
+	updatedPurchaseName,
+	updatedPurchaseAmount,
+	updatedPurchaseDonation
+) {
+	const index = purchases.findIndex((purchase) => purchase.id === purchaseId);
+
+	if (index > -1) {
+		purchases[index].id = purchaseId;
+		purchases[index].name = updatedPurchaseName
+			? updatedPurchaseName
+			: purchases[index].name;
+		purchases[index].amount = updatedPurchaseAmount
+			? Number(updatedPurchaseAmount)
+			: purchases[index].amount;
+		purchases[index].donation = updatedPurchaseDonation
+			? Number(updatedPurchaseDonation)
+			: purchases[index].donation;
+		inform('Purchase successfully updated.');
+		return purchases;
+	} else {
+		inform('Purchase not found. No action taken.');
+		return purchases;
+	}
+}
+
+function destroy(purchases, purchaseId) {
+	const index = purchases.findIndex((purchase) => purchase.id === purchaseId);
+
+	if (index > -1) {
+		purchases.splice(index, 1);
+		inform('Purchase successfully removed from collection.');
+		return purchases;
+	} else {
+		inform('Purchase not found. No action taken.');
+		return purchases;
+	}
+}
+
 module.exports = {
 	create,
+	destroy,
+	edit,
 	index,
 	show,
 };
