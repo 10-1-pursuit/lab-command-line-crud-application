63de2ab89fed868ceadcd271723a40c767d95d80
JackieC1993 <jackiecheung@pursuit.org>
2023-06-19 17:37:39 -0400

    edit my helper and controller.js

---
+++ src/controller.js
@@ -0,0 +1,41 @@
+const { nanoid } = require("nanoid")
+const chalk = require("chalk")
+
+
+const inform = console.log
+
+function create() {
+    const newPurchase = {
+        id: `${nanoid(4)}`
+    };
+    return
+}
+
+function index() {
+    return
+}
+
+function view() {
+    return
+}
+
+function update() {
+    return
+}
+
+function remove() {
+    return
+}
+
+function total() {
+    return
+}
+
+module.exports = {
+    create,
+    index,
+    view,
+    update,
+    remove,
+    total
+}
